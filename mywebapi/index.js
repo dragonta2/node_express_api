b74c5d765687a1b93332f6980204629e4c62c8e6
ta2 <dragon5555555@gmail.com>
2020-03-14 18:07:58 +0900

    JSONを返してみよう

--- mywebapi/index.js
+++ mywebapi/index.js
@@ -4,8 +4,18 @@ const express = require('express');
 // expressアプリを生成
 const app = express();
 
-// ルート(http://localhost/）にアクセスしてきたときにHelloを返す
-app.get('/', (req, res) => res.send('Hello'));
+// ルート(http://localhost:3000/api/vi/list）にアクセスしてきたときにHelloを返す
+app.get('/api/v1/list', (req, res) => {
+
+  const todoList = [
+    { title: 'Javascriptを勉強する', done: true },
+    { title: 'Node.jsを勉強する', done: false },
+    { title: 'Web APIを作る', done: false }
+  ];
+
+  // jsonを送信する
+  res.json(todoList);
+});
 
 // ポート3000でサーバーを立てる
 app.listen(3000, () => console.log('Listening on port 3000'));
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Node.jsとExpressでWeb APIを作ってみよう
+
+## Node.jsとExpressでWeb APIを作ってみよう サイトURL
+[https://sbfl.net/blog/2018/08/25/nodejs-express-webapi/](https://sbfl.net/blog/2018/08/25/nodejs-express-webapi/)
+
+## リポジトリ
+https://github.com/dragonta2/node_express_api.git
+
+### リポジトリを作成
+git remote add origin https://github.com/dragonta2/node_express_api.git
+
+
+### URLメモ
+
+#### JSONサーバー
+http://localhost:3000/api/v1/list
+
+
+#### コマンドメモ
+
+##### nodeサーバーを起動
+node index.js
+
+
+
+#### gitコマンドメモ
+git commit -m 'helloをlocalhost:3000で表示'
+
+git commit -m 'readme.mdを追加'
